4c4fc231639f6d0b86e83024f2decbeb5daf7062
Lishu Gupta <lishugupta652@gmail.com>
2024-11-19 16:14:50 +0530

    fix: build issue

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,6 +1,8 @@
 "use client";
 import {KindeProvider} from "@kinde-oss/kinde-auth-nextjs";
 
+/* @ts-ignore */
 export const AuthProvider = ({children}) => {
+  /* @ts-ignore */
   return <KindeProvider>{children}</KindeProvider>;
 };
