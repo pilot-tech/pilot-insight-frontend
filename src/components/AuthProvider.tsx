24045c140384b8913d49c30949fbe8745648a150
Devashish <devashish@pilot-tech.co>
2024-11-19 18:20:35 +0530

    build fixes

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,6 +1,12 @@
 "use client";
-import {KindeProvider} from "@kinde-oss/kinde-auth-nextjs";
 
-export const AuthProvider = ({children}) => {
+import React from "react";
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   return <KindeProvider>{children}</KindeProvider>;
 };
